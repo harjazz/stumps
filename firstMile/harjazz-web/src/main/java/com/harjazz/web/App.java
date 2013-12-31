0cd6a887f4ba691ad7925efe5501e87bc1ccec9a
nitesh.kumar <karnniteshsa@gmail.com>
2014-01-01 01:38:16 +0530

    firstMile->Sprint0

---
+++ firstMile/harjazz-web/src/main/java/com/harjazz/web/App.java
@@ -0,0 +1,13 @@
+package com.harjazz.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
